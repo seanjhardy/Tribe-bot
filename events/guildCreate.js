4cb2d31c57e4fec8b98cd99443855a3e5a62e2bf
Blumuk <mjowareltd@gmail.com>
2022-12-10 22:14:55 +0000

    removed majority of boilerplate

--- events/guildCreate.js
+++
@@ -1,6 +0,0 @@
-const logger = require("../modules/logger.js");
-// This event executes when a new guild (server) is joined.
-
-module.exports = (client, guild) => {
-  logger.log(`[GUILD JOIN] ${guild.id} added the bot. Owner: ${guild.ownerId}`);
-};
