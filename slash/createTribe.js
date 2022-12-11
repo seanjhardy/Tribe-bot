a2ad042f2c69dedb288f88fb1ddf4c1fbff81242
faderz <58748338+faderzz@users.noreply.github.com>
2022-12-11 18:44:12 +0000

    discord.js is so ass

--- slash/createTribe.js
+++ slash/createTribe.js
@@ -1,24 +1,100 @@
 const { Permissions } = require("discord.js");
-//import enmap and open the enmap.sqlite database in ../data/tribes.sqlite
-const Enmap = require("enmap");
-const tribes = new Enmap({ name: "tribes" });
+
+const Enmap = require("enmap"); // Import Enmap
+const tribes = new Enmap({ name: "tribes" }); // Create an instance of Enmap - Tribes
 
 exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
   // CreateTribe Command
   const name = interaction.options.getString("name");
   const emoji = interaction.options.getString("emoji");
-  if (tribes.has(name)) { // Check if tribes exists
-    return interaction.reply("This tribe name is already taken");
+
+  if (tribes.has(name)) { // Check if tribes exists with name or emoji
+    return interaction.reply(`Tribe name or emoji is already taken.`);
   } else {
-    tribes.set(name, emoji);
-    interaction.guild.roles.create({
-      data: {
-        name: name,
-        color: 0,
-        permissions: [],
-      },
+    //create a role called name
+    const role = await interaction.guild.roles.create({
+        name: interaction.options.getString("name"),
     });
-    return interaction.reply(`Tribe "**${name}**" with emoji **${emoji}** created`);
+
+    const roleID = role.id;
+    console.log(emoji);
+
+    // if emoji is null, categoryName is name else categoryName is emoji + name
+    if (emoji == null) {
+      var categoryName = name;
+    } else {
+      var categoryName = emoji;
+    }
+
+    // Create Category and Channels.
+    const category = await interaction.guild.channels.create(categoryName, {
+      type: "GUILD_CATEGORY",
+      permissionOverwrites: [
+        {
+          id: interaction.guild.id,
+          deny: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+        {
+          id: roleID,
+          allow: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+      ],
+    });
+    const announcements = await interaction.guild.channels.create("Announcements", {
+      type: "GUILD_TEXT",
+      parent: category,
+      permissionOverwrites: [
+        {
+          id: interaction.guild.id,
+          deny: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+        {
+          id: roleID,
+          allow: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+        {
+          id: roleID,
+          deny: [Permissions.FLAGS.SEND_MESSAGES],
+        },
+      ],
+    });
+    const general = await interaction.guild.channels.create("General", {
+      type: "GUILD_TEXT",
+      parent: category,
+      permissionOverwrites: [
+        {
+          id: interaction.guild.id,
+          deny: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+        {
+          id: roleID,
+          allow: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+      ],
+    });
+    const vc1 = await interaction.guild.channels.create("VC 1", {
+      type: "GUILD_VOICE",
+      parent: category,
+      permissionOverwrites: [
+        {
+          id: interaction.guild.id,
+          deny: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+        {
+          id: roleID,
+          allow: [Permissions.FLAGS.VIEW_CHANNEL],
+        },
+      ],
+    });
+
+    // Add to Enmap
+    tribes.set(name, {
+      name: name,
+      emoji: emoji,
+      roleID: roleID,
+    });
+    // Send message
+    return interaction.reply(`Tribe "**${name}**" has been created.`);
   }
 };
 
@@ -35,12 +111,10 @@ exports.commandData = {
 },
 {
     name:"emoji",
-    description:"the emoji of the tribe you want to create",
+    description:"**OPTIONAL** the emoji of the tribe you want to create",
     type:3,
-    required:true
+    required:false
 }
-
-
   ],
   defaultPermission: true,
 };
