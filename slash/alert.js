b657bde93b6adb239b09392571a52c19d0a61909
faderz <58748338+faderzz@users.noreply.github.com>
2022-12-23 13:06:27 +0000

    Update alert.js

--- slash/alert.js
+++ slash/alert.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-undef */
 /* eslint-disable linebreak-style */
-const { Permissions } = require("discord.js");
+const { Permissions, EmbedBuilder } = require("discord.js");
 
 const { ReadData, StoreTribe } = require("../modules/functions");
 require("dotenv").config();
@@ -13,6 +14,58 @@ exports.run = async (client, interaction) => { // eslint-disable-line no-unused-
   // Get message object.
   message = await interaction.channel.messages.fetch(messageID);
   messageContent = message.content; // Message content
+  messageAuthorID = message.author.id; // Message author ID
+  messageAuthorUsername = message.author.username + "#" + message.author.discriminator; // Message author ID
+  messageChannelID = message.channel.id; // Message channel ID
+  messageCategoryID = message.channel.parent.id; // Message category ID
+
+  alertChannel = process.env.ALERT_CHANNEL; // Alert channel ID
+
+  reporterID = interaction.user.id; // Reporter ID
+  
+  // Roles of person reported
+  victimRoles = interaction.guild.members.cache.get(messageAuthorID).roles.cache.map(r => r.name);
+
+  // Roles of person reporting
+  executorRoles = interaction.member.roles.cache.map(r => r.name);
+
+  // Find common roles between users
+  commonRoles = victimRoles.filter(value => executorRoles.includes(value));
+    
+  // Check if any of common roles are in tribedata.
+  var tribe = "";
+  var tribeCategory = "";
+  var tribeRoleID = "";
+  for (var i = 0; i < commonRoles.length; i++) {
+    if (tribedata.tribes[commonRoles[i]]) {
+      tribe = commonRoles[i];
+      tribeCategory = tribedata.tribes[tribe].Category;
+      tribeRoleID = tribedata.tribes[tribe].RoleID;
+      break;
+    }
+  }
+
+  // Check if message is in tribeCategory
+  if (tribe == "" || message.channel.parent != tribeCategory) {
+    interaction.reply({ content: "You can only alert the mods of a message in your tribe category.", ephemeral: true }); // Send message to user
+    return;
+  }
+
+  // Send message to alert channel
+  const embed = {
+    "title": `Alert -  ${tribe}`,
+    "description": `**User Reported:** <@${messageAuthorID}>\n**Tribe:** ${tribe}\n**Reported by:** <@${interaction.user.id}>\n**Message:** ${messageContent}\n**Message Link:** [Click Here](https://discord.com/channels/${interaction.guild.id}/${messageChannelID}/${messageID})`,
+    "color": 16711680, // 16711680 = red for moderation logs | 4690898 = pink/purplish for other commands
+    "timestamp": new Date(),
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/icons/811270187843977236/5a7ac443be8f92675def615e470ac4a6.webp?size=96",
+      "text": "Hamza's Cult"
+    }
+  };
+
+  client.channels.cache.get(process.env.alertChannel).send({ "content": `<@&${tribeRoleID}>`, embeds: [embed] });
+
+  return interaction.reply("Alerted the mods of the message. Thank you for your report.")
 };
 
 exports.commandData = {
