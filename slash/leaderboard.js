3399427b9e457ef5827d805c29763c6f7171b75d
faderz <58748338+faderzz@users.noreply.github.com>
2023-03-04 18:00:00 +0000

    Update leaderboard.js

--- slash/leaderboard.js
+++ slash/leaderboard.js
@@ -1,18 +1,26 @@
 /* eslint-disable linebreak-style */
-const { ReadData, MinusPoints } = require("../modules/functions");
+const { ReadData } = require("../modules/functions");
 require("dotenv").config();
 
 exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
-  await interaction.deferReply({ ephemeral: true });
+  await interaction.deferReply({ ephemeral: false });
   // JSON file
   var tribedataraw = await ReadData();
   var tribedata = JSON.parse(tribedataraw);
 
-  // Get top 10 tribes with the most points.
-  var top10 = Object.keys(tribedata.tribes).sort((a, b) => tribedata.tribes[b].Points - tribedata.tribes[a].Points).slice(0, 10);
+  // Get top 10 tribes with the most points and their number of points.
+  var top10 = Object.entries(tribedata.tribes).sort((a, b) => b[1].points - a[1].points).slice(0, 10);
   console.log(top10)
 
-  // No log needed for this command.
+  // Create embed with top 10 tribes.
+  const embed = new MessageEmbed()
+    .setColor(4690898)
+    .setTitle("Leaderboard")
+    .addField(`🥇**1** ${top10}.`, `Points: ${top10}`, true)
+    .setFooter("Hamza's Cult • Tribe System", "https://cdn.discordapp.com/icons/811270187843977236/5a7ac443be8f92675def615e470ac4a6.webp?size=96")
+
+  // return the embed to user
+  await interaction.reply({embeds: [embed]});
 };
 
 exports.commandData = {
@@ -27,4 +35,3 @@ exports.conf = {
   permLevel: "User",
   guildOnly: true
 };
-//Tested
