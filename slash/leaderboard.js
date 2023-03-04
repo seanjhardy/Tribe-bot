43787361882e5ee91d8617b163b013a2a8f187f4
faderz <58748338+faderzz@users.noreply.github.com>
2023-03-04 20:44:50 +0000

    wayyyy

--- slash/leaderboard.js
+++ slash/leaderboard.js
@@ -3,24 +3,38 @@ const { ReadData } = require("../modules/functions");
 require("dotenv").config();
 
 exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
-  await interaction.deferReply({ ephemeral: false });
   // JSON file
   var tribedataraw = await ReadData();
   var tribedata = JSON.parse(tribedataraw);
 
-  // Get top 10 tribes with the most points and their number of points.
-  var top10 = Object.entries(tribedata.tribes).sort((a, b) => b[1].points - a[1].points).slice(0, 10);
-  console.log(top10)
+  tribes = tribedata.tribes
+  // Sort tribes by points in ascending order
+  const sortedTribes = Object.entries(tribes)
+  .sort(([, a], [, b]) => b.Points - a.Points)
+  .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
 
-  // Create embed with top 10 tribes.
-  const embed = new MessageEmbed()
-    .setColor(4690898)
-    .setTitle("Leaderboard")
-    .addField(`🥇**1** ${top10}.`, `Points: ${top10}`, true)
-    .setFooter("Hamza's Cult • Tribe System", "https://cdn.discordapp.com/icons/811270187843977236/5a7ac443be8f92675def615e470ac4a6.webp?size=96")
+  const sortedKeys = Object.keys(sortedTribes);
 
-  // return the embed to user
-  await interaction.reply({embeds: [embed]});
+
+  // Create function to conntattonate string
+  function concatString(index) {
+    return `${index + 1}. ${sortedKeys[index]} - **${tribes[sortedKeys[index]].Points} points**\n`;
+  }
+  console.log(sortedKeys)
+  // Create embed
+  const embed = {
+    "title": "Banish",
+    "description": `🥇1. ${sortedKeys[0]} - **${tribes[sortedKeys[0]].Points} points**` + `🥈2. ${sortedKeys[1]} - **${tribes[sortedKeys[1]].Points} points**`,
+    "color": 16711680, // 16711680 = red for moderation logs | 4690898 = pink/purplish for other commands
+    "timestamp": new Date(),
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/icons/811270187843977236/5a7ac443be8f92675def615e470ac4a6.webp?size=96",
+      "text": "Hamza's Cult"
+    }
+  };
+  // Send leaderboard
+  interaction.reply({ embeds: [embed] });
+
 };
 
 exports.commandData = {
